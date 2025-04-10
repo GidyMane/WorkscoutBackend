96977636b11268f9a63dc2d34347ccaac5a07aa4
GidyMane <muragegideon2000@gmail.com>
2025-04-10 16:36:33 +0300

    fix routes

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(compression());
 app.use(cookieParser());
-app.use("/api/v1/", routes)
+app.use("/api/v1", routes)
 
 // Define a route for the root path ('/')
 app.get('/', (req: Request, res: Response) => {
